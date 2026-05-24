71d66bd77a58117f3711817eb04368487534214b
Omar Arias <omararias870503@gmail.com>
2026-05-23 20:26:51 -0400

    Agregando cambios en carpeta src

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,10 @@
-function Card() {
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+function Card(props: Props) {
+  const { children } = props;
   return (
     <div
       className="card"
@@ -6,24 +12,21 @@ function Card() {
         width: "350px",
       }}
     >
-      <div className="card-body">
-        <CardBody />
-      </div>
+      <div className="card-body">{children}</div>
     </div>
   );
 }
 
-export function CardBody() {
+interface CardBodyProps {
+  title: string;
+  text?: string;
+}
+export function CardBody(props: CardBodyProps) {
+  const { title, text } = props;
   return (
     <>
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card’s content.
-      </p>
-      <a href="#" className="btn btn-primary">
-        Go somewhere
-      </a>
+      <h5 className="card-title">{title}</h5>
+      <p className="card-text">{text}</p>
     </>
   );
 }
