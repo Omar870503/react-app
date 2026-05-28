3112373647dd37ad50c0e2961e1e59c4d8afa0cb
Omar Arias <omararias870503@gmail.com>
2026-05-27 21:47:35 -0400

    Agregando cambios a carpeta components y archivo Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import Card, { CardBody } from "./components/Card";
+import Button from "./components/Button";
 import List from "./components/List";
 
 function App() {
-  const list = ["Goku", "Tanjiro", "Eren"];
+  const list: string[] = ["Goku", "Vegeta", "Gohan", "Trunks"];
   const handleSelect = (elemento: string) => {
     console.log("Imprimiendo", elemento);
   };
+
+  const contenido = list.length !== 0 && (
+    <List data={list} onSelect={handleSelect}></List>
+  );
+
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el texto" />
-      <List data={list} onSelect={handleSelect}></List>
+      {contenido}
+      <Button>Hola Mundo</Button>
     </Card>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+function Button({ children }: Props) {
+  return (
+    <button type="button" className="btn btn-primary">
+      {children}
+    </button>
+  );
+}
+
+export default Button;
