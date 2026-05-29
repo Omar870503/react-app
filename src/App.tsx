f342c5524a892021451ee7c22a9628b7e1c81380
Omar Arias <omararias870503@gmail.com>
2026-05-29 17:12:12 -0400

    Agregando cambios

--- src/App.tsx
+++ src/App.tsx
@@ -34,8 +34,12 @@ function App() {
   const delMinion = () => setData(data.slice(0, -1));
   return (
     <Card>
-      <Button onClick={addMinion}>Agregar</Button>
-      <Button onClick={delMinion}>Eliminar</Button>
+      <Button isLoading={true} onClick={addMinion}>
+        Agregar
+      </Button>
+      <Button isLoading={false} onClick={delMinion}>
+        Eliminar
+      </Button>
       <List data={data} />
     </Card>
   );
